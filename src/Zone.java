5c8a7d5c55f47f68d624ae4bab3b0c3558b4b9a9
tvasiliadis <tvasiliadis@ots.gr>
2023-03-29 10:44:46 +0300

    zone

--- src/Zone.java
+++ src/Zone.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zone {
+    private String name;
+    private List<Slot> slotList = new ArrayList<>();
+    public Zone(String name){
+        this.name=name;
+    }
+
+    
+    public void addSlot(Slot slot){
+        slotList.add(slot);
+    }
+
 }
