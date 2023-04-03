e367655a9dc8869abdf583b9fd5561838c0e628f
tvasiliadis <tvasiliadis@ots.gr>
2023-04-03 12:02:52 +0300

    parking,test,zone

--- src/java/Tests/PriceListTest.java
+++ src/java/Tests/PriceListTest.java
@@ -10,40 +10,40 @@ class PriceListTest {
 
 
 
-//    @Test
-//    void PriceListTest(){
-//        PriceList pricelist = new PriceList(1);
-//        ParkingStop pk = new ParkingStop("aSD");
-//        pk.setEntry(LocalTime.of(12,01,00));
-//        pk.setExit(LocalTime.now());
-//        pricelist.addPriceScale(120,20,1);
-//        pricelist.addPriceScale(1440,60,3);
-//        pricelist.addPriceScale(2880,240,2);
-//        assertEquals(18,pricelist.calculate_cost(pricelist.calculate_cost(pk.getDuration())));
-//
-//    }
-//
-//    @Test
-//    void PriceListTestMinus(){
-//        PriceList pricelistMinus = new PriceList(2);
-//        pricelistMinus.addPriceScale(-120,10,-10);
-//        pricelistMinus.calculate_cost(240);
-//
-//    }
-//    @Test
-//    void PriceListTestDouble(){
-//        PriceList pricelistDouble = new PriceList(3);
-//        pricelistDouble.addPriceScale(245,25,1);
-//        pricelistDouble.addPriceScale(755,13,3);
-//        pricelistDouble.calculate_cost(240);
-//    }
-//
-//    @Test
-//    void PriceListEndScaleTest(){
-//        PriceList pricelistDouble = new PriceList(3);
-//        pricelistDouble.addPriceScale(245,250,1);
-//        pricelistDouble.calculate_cost(240);
-//    }
+    @Test
+    void PriceListTest(){
+        PriceList pricelist = new PriceList(1);
+        ParkingStop pk = new ParkingStop("aSD");
+        pk.setEntry(LocalTime.of(12,01,00));
+        pk.setExit(LocalTime.now());
+        pricelist.addPriceScale(120,20,1);
+        pricelist.addPriceScale(1440,60,3);
+        pricelist.addPriceScale(2880,240,2);
+        assertEquals(18,pricelist.calculate_cost(pricelist.calculate_cost(pk.getDuration())));
+
+    }
+
+    @Test
+    void PriceListTestMinus(){
+        PriceList pricelistMinus = new PriceList(2);
+        pricelistMinus.addPriceScale(-120,10,-10);
+        pricelistMinus.calculate_cost(240);
+
+    }
+    @Test
+    void PriceListTestDouble(){
+        PriceList pricelistDouble = new PriceList(3);
+        pricelistDouble.addPriceScale(245,25,1);
+        pricelistDouble.addPriceScale(755,13,3);
+        pricelistDouble.calculate_cost(240);
+    }
+
+    @Test
+    void PriceListEndScaleTest(){
+        PriceList pricelistDouble = new PriceList(3);
+        pricelistDouble.addPriceScale(245,250,1);
+        pricelistDouble.calculate_cost(240);
+    }
 
 
 
